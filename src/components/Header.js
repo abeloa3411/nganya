4084c95ac32275392ff4df64f96ada1bf0dc9ffe
abel <abelw34@gmail.com>
2022-09-21 14:16:37 +0300

    new navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { FaShuttleVan } from "react-icons/fa";
+import { IoIosHelp } from "react-icons/io";
 import { FiUser } from "react-icons/fi";
 import { VanState } from "../context/Context";
 
@@ -23,34 +23,41 @@ const Header = () => {
   } = VanState();
   return (
     <div className="fixed w-full">
-      <div className="bg-green-100 p-6 bg-white shadow-lg flex justify-between align-center h-20">
-        <div>
-          <h2 className="text-3xl">
+      <div className="bg-green-100 p-6 bg-white shadow-lg flex justify-between items-center h-20">
+        <div className="flex">
+          <h2 className="text-3xl mr-2">
             <Link to="/">Nganya</Link>
           </h2>
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Search van"
-            className="p-2 w-80 rounded outline-none"
-            onChange={(e) => {
-              vanDispatch({
-                type: "FILTER_BY_SEARCH",
-                payload: e.target.value,
-              });
-            }}
-          />
-        </div>
-        <div className="flex items-center">
-          <div className="pt-2 flex items-center">
-            <p className="px-2">{van.length}</p>
-            <Link to="/booking">
-              <FaShuttleVan style={{ fontSize: "25px" }} />
-            </Link>
+          <div className="flex items-center justyfy-center">
+            <ul className="mx-4">
+              <li>
+                <Link to="/">Box</Link>
+              </li>
+            </ul>
+            <ul className="mx-4">
+              <li>
+                <Link to="/">Trains</Link>
+              </li>
+            </ul>
+            <ul className="mx-4">
+              <li>
+                <Link to="/">Flights</Link>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <Link to="/">VIP</Link>
+              </li>
+            </ul>
           </div>
-          <div className="flex items-center ml-2 px-4 py-2 transition ease-in-out cursor-pointer rounded hover:bg-slate-300">
-            <Link to="/login">Login</Link>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="flex items-center">
+            <Link to="/booking">Help</Link>
+            <IoIosHelp style={{ fontSize: "25px" }} />
+          </div>
+          <div className="flex items-center ml-2 px-4 py-2">
+            <Link to="/login">Manage account</Link>
             <FiUser />
           </div>
         </div>
